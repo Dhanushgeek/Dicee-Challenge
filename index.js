40e9ca4d9cba089702c825bbfdb406081f16c7c1
Dhanushgeek <dhanushgeek@gmail.com>
2020-12-19 16:09:33 +0530

    JS update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 randomImg1();
 randomImg2();
+selectWinner();
 
 function randomImg1() {
     var randomNumber1 = Math.floor((Math.random() * 6) + 1);
@@ -9,6 +10,8 @@ function randomImg1() {
     var image1 = document.querySelectorAll("img")[0];
 
     image1.setAttribute("src", randomDiceImage);
+
+    return randomNumber1;
 }
 
 function randomImg2() {
@@ -17,4 +20,19 @@ function randomImg2() {
     var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 
     var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+    return randomNumber2;
+}
+
+function selectWinner() {
+    var randomNumber1 = randomImg1();
+    var randomNumber2 = randomImg2();
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 wins!";
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "player 2 wins";
+    } else {
+        document.querySelector("h1").innerHTML = "Game draw";
+    }
 }
